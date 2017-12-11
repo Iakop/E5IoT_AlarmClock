1980ce7ff02fad11cb5f8ded9b82450602a8b0f7
Iakop <jacob.bp@mvb.net>
2017-12-11 14:11:00 +0100

    Extracted spotify functions

--- Main_Script/main_script.py
+++ Main_Script/main_script.py
@@ -14,10 +14,6 @@ try:
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
-    
-# Import Spotify API
-import spotify # Imports Spotify methods.
-import threading # Imports Threading methods.
 
 # For time delays, and sleeping
 import time
@@ -100,34 +96,6 @@ def getCalPosts():
     for event in events:
         details = event['attachments'].get('title', event['start'].get('date'))
         lcd.message(details)
-
-# A login state tracker thread.
-def spotifyConnectionState(session):
-    if session.connection.state is spotify.ConnectionState.LOGGED_IN:
-	logged_in_event.set()	
-
-# Logs into Spotify
-def spotifyLogIn():
-    loop.start() # Starts the spotify session loop.
-    session.on(
-	spotify.SessionEvent.CONNECTION_STATE_UPDATED,
-	connection_state_listener)
-    session.login('1217631550', 's3cretpassword')
-    logged_in_event.wait()
-
-# Plays random Spotify songs from playlist. Query format = u'Discover Weekly'
-def spotifyPlayRandom(playlistQuery):
-    # Load the playlist from search query.
-    playlist = spotify.search(session, query=playlistQuery).load(timeout=60) # Search with timeout of 60s.
-    tracks = playlist.tracks()
-    playlist.load(60) # Timeout the loading in 60s.
-    playTrack = random.randint(1,len(tracks)) # Randomly pick the track to be played.
-
-    # Play the specific Track:
-    track = session.get_track(playTrack)
-    track.load()
-    session.player.load(track)
-    session.player.play()
 
 # 1 - On boot.
 # Get the Calendar data from Google Calendar's API
---
+++ Main_Script/spoti.py
@@ -0,0 +1,31 @@
+# Import Spotify API
+import spotify # Imports Spotify methods.
+import threading # Imports Threading methods.
+
+# A login state tracker thread.
+def spotifyConnectionState(session):
+    if session.connection.state is spotify.ConnectionState.LOGGED_IN:
+	logged_in_event.set()	
+
+# Logs into Spotify
+def spotifyLogIn():
+    loop.start() # Starts the spotify session loop.
+    session.on(
+	spotify.SessionEvent.CONNECTION_STATE_UPDATED,
+	connection_state_listener)
+    session.login('1217631550', 's3cretpassword')
+    logged_in_event.wait()
+
+# Plays random Spotify songs from playlist. Query format = u'Discover Weekly'
+def spotifyPlayRandom(playlistQuery):
+    # Load the playlist from search query.
+    playlist = spotify.search(session, query=playlistQuery).load(timeout=60) # Search with timeout of 60s.
+    tracks = playlist.tracks()
+    playlist.load(60) # Timeout the loading in 60s.
+    playTrack = random.randint(1,len(tracks)) # Randomly pick the track to be played.
+
+    # Play the specific Track:
+    track = session.get_track(playTrack)
+    track.load()
+    session.player.load(track)
+    session.player.play()
