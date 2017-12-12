89677ec330db0e3d858ead59dfabbf3a2c653dd3
Jacob Bechmann Pedersen <jacob.bp@mvb.net>
2017-12-12 22:45:16 +0100

    Fixed FSM example

--- Examples/fsm_example.py
+++ Examples/fsm_example.py
@@ -7,11 +7,13 @@ import time
 from fysom import Fysom
 
 global fsm
+global alarmFlag
+
 fsm = Fysom({
   'initial': 'start',
   'events': [
     {'name': 'showWelcome', 'src': 'start', 'dst': 'welcome'},
-    {'name': 'showMenu', 'src': ['welcome','songStart','weatherAPI','calendarAPI'], 'dst': 'menu'},
+    {'name': 'showMenu', 'src': ['welcome','startSong','weatherAPI','calendarAPI'], 'dst': 'menu'},
     {'name': 'alarmTrigger', 'src': 'menu', 'dst': 'startSong'},
     {'name': 'alarmTrigger', 'src': 'startSong', 'dst': 'weatherAPI'},
     {'name': 'getCalendar', 'src': 'menu', 'dst': 'calendarAPI'},
@@ -24,36 +26,44 @@ fsm = Fysom({
 })
 
 def main():
+    alarmFlag = False
     while True:
-        if fsm.isstate('start')
+        if fsm.isstate('start'):
             print(fsm.current)
             time.sleep(1)
             fsm.showWelcome()
-        
-        elif fsm.isstate('welcome')
-            print(fsm.current)
-            time.sleep(1)
-            fsm.showMenu()
-            
-        elif fsm.isstate('menu')
-            print(fsm.current)
-            time.sleep(1)
-            fsm.alarmTrigger()
 
-        elif fsm.isstate('calendarAPI')
+        elif fsm.isstate('welcome'):
             print(fsm.current)
             time.sleep(1)
             fsm.showMenu()
-        
-        elif fsm.isstate('weatherAPI')
+
+        elif fsm.isstate('menu'):
+            print(fsm.current)
+            time.sleep(1)
+            if alarmFlag == True:
+                fsm.alarmTrigger()
+
+        elif fsm.isstate('calendarAPI'):
             print(fsm.current)
             time.sleep(1)
             fsm.showMenu()
-        
-        elif fsm.isstate('startSong')
+
+        elif fsm.isstate('weatherAPI'):
             print(fsm.current)
             time.sleep(1)
             fsm.showMenu()
-    
+
+        elif fsm.isstate('startSong'):
+            print(fsm.current)
+            time.sleep(1)
+            if alarmFlag == True:
+                alarmFlag = False
+                fsm.alarmTrigger()
+            else:
+                fsm.showMenu()
+
+        alarmFlag = True
+
 if __name__ == '__main__':
     main()
